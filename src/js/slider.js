cde4b34410ed64d1b55c7501bdad4a2aeaaab139
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-03-05 18:47:25 +0100

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,10 +5,16 @@
         slidesToShow: 3,
         slidesToScroll: 3
       });
-      
 
       $('.multiple-items-one').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1
+      });
+      
+
+      $('.multiple-items-two').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2
       });
