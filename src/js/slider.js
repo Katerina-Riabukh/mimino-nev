91712966c1efbceaed3d8cf456d92e7ab5ae0329
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-03-06 19:01:00 +0100

    slider for hero

--- src/js/slider.js
+++ src/js/slider.js
@@ -10,22 +10,22 @@ $('.slider').slick({
   infinite: true,
   variableWidth: true,
   autoplay: false,
-  responsive: [
-    {
-      breakpoint: 375,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        // dots: true
-      }
-    },
+  // responsive: [
+  //   {
+  //     breakpoint: 375,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       slidesToScroll: 1,
+  //       infinite: true,
+  //       // dots: true
+  //     }
+  //   },
     
     
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+  //   // You can unslick at a given breakpoint now by adding:
+  //   // settings: "unslick"
+  //   // instead of a settings object
+  // ]
 });
     
 
