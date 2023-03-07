5dd7579031c9086808fb3f38c3fece00d8ebb99c
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-03-07 11:45:28 +0100

    slider css

--- src/js/slider.js
+++ src/js/slider.js
@@ -21,24 +21,71 @@ $('.slider').slick({
   infinite: true,
   variableWidth: true,
   autoplay: false,
-  // responsive: [
-  //   {
-  //     breakpoint: 375,
-  //     settings: {
-  //       slidesToShow: 2,
-  //       slidesToScroll: 1,
-  //       infinite: true,
-  //       // dots: true
-  //     }
-  //   },
-    
-    
-  //   // You can unslick at a given breakpoint now by adding:
-  //   // settings: "unslick"
-  //   // instead of a settings object
-  // ]
+ 
 });
+
+$('.slider-1').slick({
+  dots: false,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  adaptiveWidth: true,
+  adaptiveHeigth: true,
+  lazyLoad: 'ondemand',
+  infinite: true,
+  variableWidth: true,
+  autoplay: false,
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        // dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        // dots: true
+      }
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        // dots: true
+      }
+    },
     
+    
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.slider-2').slick({
+  dots: false,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  adaptiveWidth: true,
+  adaptiveHeigth: true,
+  lazyLoad: 'ondemand',
+  infinite: true,
+  variableWidth: true,
+  autoplay: false,
+ 
+});
 
       
 
